0a2c1f11cc95b3f24ceafd3425f75a88cc8f55ec
dhekoz <petrus.d.r.soge@gmail.com>
2022-11-08 00:32:11 +0700

    use-the-spread-operator-to-evaluate-arrays-in-place

---
+++ freecodecamp/ES6/use-the-spread-operator-to-evaluate-arrays-in-place.js
@@ -0,0 +1,12 @@
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1];  // Change this line
+
+console.log(arr2);
+
+// var arr = [ 4, 5, 89, 90];
+// var maximum = Math.max.apply(null, arr);
+
+// const arr = [ 90, 493, 232, 78];
+// const maximum = Math.max(...arr);
