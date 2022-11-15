f4c7d9b34c105dc690c6b608116b5a61b0aeab47
dhekoz <petrus.d.r.soge@gmail.com>
2022-11-15 08:43:42 +0700

    new save

--- freecodecamp/Regular Expressions/match-all-letters-and-numbers.js
+++ freecodecamp/Regular Expressions/match-all-letters-and-numbers.js
@@ -1,3 +1,9 @@
+// The closest character class in JavaScript to match the alphabet is \w. This shortcut is equal to [A-Za-z0-9_]. This character class matches upper and lowercase letters plus numbers. Note, this character class also includes the underscore character (_).
+
+
+// Use the shorthand character class \w to count the number of alphanumeric characters in various quotes and strings.
+
+
 let quoteSample = "The five boxing wizards jump quickly.";
 let alphabetRegexV2 = /[A-Za-z0-9_\w+]/g; // Change this line
 let result = quoteSample.match(alphabetRegexV2).length;
