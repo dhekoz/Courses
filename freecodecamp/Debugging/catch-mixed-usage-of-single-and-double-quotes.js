4e338b35ed9a498d7d53cfd6d8271675dd5658d8
dhekoz <petrus.d.r.soge@gmail.com>
2022-11-23 09:21:58 +0700

    catch mixed usage of single and double quotes

---
+++ freecodecamp/Debugging/catch-mixed-usage-of-single-and-double-quotes.js
@@ -0,0 +1,7 @@
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+console.log(innerHtml);
+
+// or
+
+let innerHtml2 = '<p>Click here to <a href="#Home">return home</a></p>';
+console.log(innerHtml);
