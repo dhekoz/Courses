292b5f95b480843a59911e96c3d3ee9c15604040
dhekoz <petrus.d.r.soge@gmail.com>
2022-11-24 12:36:56 +0700

    new

--- freecodecamp/Debugging/use-caution-when-reinitializing-variables-inside-a-loop.js
+++ freecodecamp/Debugging/use-caution-when-reinitializing-variables-inside-a-loop.js
@@ -1,8 +1,12 @@
+/*
+The following function is supposed to create a two-dimensional array with m rows and n columns of zeroes. Unfortunately, it's not producing the expected output because the row variable isn't being reinitialized (set back to an empty array) in the outer loop. Fix the code so it returns a correct 3x2 array of zeroes, which looks like [[0, 0], [0, 0], [0, 0]].
+*/
+
 function zeroArray(m, n) {
   // Creates a 2-D array with m rows and n columns of zeroes
   let newArray = [];
   for (let i = 0; i < m; i++) {
-  let row = [];
+    let row = [];
     // Adds the m-th row into newArray
 
     for (let j = 0; j < n; j++) {
