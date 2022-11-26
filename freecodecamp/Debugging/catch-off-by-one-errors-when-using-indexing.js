69903154a11f27a369763d45db34951238e2edab
dhekoz <petrus.d.r.soge@gmail.com>
2022-11-26 23:41:15 +0700

    new

--- freecodecamp/Debugging/catch-off-by-one-errors-when-using-indexing.js
+++ freecodecamp/Debugging/catch-off-by-one-errors-when-using-indexing.js
@@ -1,3 +1,11 @@
+/*
+Off by one errors (sometimes called OBOE) crop up when you're trying to target a specific index of a string or array (to slice or access a segment), or when looping over the indices of them. JavaScript indexing starts at zero, not one, which means the last index is always one less than the length of the item. If you try to access an index equal to the length, the program may throw an "index out of range" reference error or print undefined.
+
+When you use string or array methods that take index ranges as arguments, it helps to read the documentation and understand if they are inclusive (the item at the given index is part of what's returned) or not. Here are some examples of off by one errors:
+
+Fix the two indexing errors in the following function so all the numbers 1 through 5 are printed to the console.
+*/
+
 function countToFive() {
   let firstFive = "12345";
   let len = firstFive.length;
