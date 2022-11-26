b7c5eaedea7639a05b10b84643e37bddbb47561d
dhekoz <petrus.d.r.soge@gmail.com>
2022-11-26 23:15:49 +0700

    use-an-array-to-store-a-collection-of-data

---
+++ freecodecamp/basicDataStructure/use-an-array-to-store-a-collection-of-data.js
@@ -0,0 +1,4 @@
+/*
+We have defined a variable called yourArray. Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. Your array should contain at least one string, one number, and one boolean.
+*/
+let yourArray = ["Chester", 87, true, false, "harem"]; // Change this line
