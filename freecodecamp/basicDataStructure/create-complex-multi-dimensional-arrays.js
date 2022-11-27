d2bc4fcafdf5a815e37c4ee6a6d88aacc6303cfd
dhekoz <petrus.d.r.soge@gmail.com>
2022-11-27 19:51:27 +0700

    create complex multi dimensional arrays

---
+++ freecodecamp/basicDataStructure/create-complex-multi-dimensional-arrays.js
@@ -0,0 +1,15 @@
+let myNestedArray = [
+  // Only change code below this line
+  // ['unshift', false, 1, 2, 3, 'complex','nested'],
+  // ['loop', 'shift', 6, 7, 1000,'method'],
+  // ['concat', false, true, 'spread', 'array', ['deep']],
+  // ['mutate', 1327.98, 'splice', 'slice', 'push', [['deeper']]],
+  // ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth', [[['deepest']]]]
+  // or
+  "level 1",
+  ["level 2"],
+  [["level 3", "deep"]],
+  [[["level 4", "deeper"]]],
+  [[[["level 5", "deepest"]]]],
+  // Only change code above this line
+];
