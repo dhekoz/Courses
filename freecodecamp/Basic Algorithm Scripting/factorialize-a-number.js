8635a44c1b5a0d794d83d7d7f8944ff0b8a18828
dhekoz <petrus.d.r.soge@gmail.com>
2022-12-01 07:56:39 +0700

    factorialize-a-number

--- freecodecamp/Basic Algorithm Scripting/factorialize-a-number.js
+++ freecodecamp/Basic Algorithm Scripting/factorialize-a-number.js
@@ -1,13 +1,38 @@
 function factorialize(num) {
   let result = num;
   if (num === 0 || num === 1) return 1;
-  while(num > 1){
+  while (num > 1) {
     num--;
-  
-  result *= num;
 
+    result *= num;
   }
   return result;
 }
+factorialize(5);
+//  OR
+/*function factorialize(num) {
+  if (num === 0 || num === 1)
+  return 1;
+  for(let i = num - 1; i >= 1; i--){
+    num = num * i;
+  }
+   return num;
+ }
+ 
+ factorialize(5); */
+
+//  OR
+
+/*
+function factorialize(num) {
+ if (num < 0) 
+ return -1;
+ else if(num === 0) 
+ return 1;
+ else {
+   return (num * factorialize(num - 1));
+ }
+}
 
 factorialize(5);
+*/
