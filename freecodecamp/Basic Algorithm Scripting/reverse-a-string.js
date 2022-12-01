d069d98b75783b0da82694c8eeb6c69a43f1da91
dhekoz <petrus.d.r.soge@gmail.com>
2022-12-01 07:30:44 +0700

    reverse-a-string

--- freecodecamp/Basic Algorithm Scripting/reverse-a-string.js
+++ freecodecamp/Basic Algorithm Scripting/reverse-a-string.js
@@ -3,3 +3,11 @@ function reverseString(str) {
 }
 
 reverseString("hello");
+
+/*
+Reverse the provided string.
+
+You may need to turn the string into an array before you can reverse it.
+
+Your result must be a string.
+*/
