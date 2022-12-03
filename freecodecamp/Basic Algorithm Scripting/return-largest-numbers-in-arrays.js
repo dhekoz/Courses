e754fff6438190a34b4fb96c6c5bc12e5467b1d4
dhekoz <petrus.d.r.soge@gmail.com>
2022-12-03 23:12:58 +0700

    return largest numbers in arrays 4th solutions

--- freecodecamp/Basic Algorithm Scripting/return-largest-numbers-in-arrays.js
+++ freecodecamp/Basic Algorithm Scripting/return-largest-numbers-in-arrays.js
@@ -82,3 +82,15 @@ Since, Math.max is independent of any context, hence, it ignores the bogus conte
 
 Thus, our Function.prototype.apply.bind(Math.max, null) makes a new function accepting the arr.map values i.e. the inner arrays.
  */
+
+
+//OR USING RECURSIVE APPROACH
+
+/*
+function largestOfFour(arr, finalArr = []) {
+  return !arr.length
+    ? finalArr
+    : largestOfFour(arr.slice(1), finalArr.concat(Math.max(...arr[0])))
+}
+
+*/
