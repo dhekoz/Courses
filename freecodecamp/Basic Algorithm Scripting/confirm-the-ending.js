56ab69b42a2dec37d5ed763e18608579f365ea60
dhekoz <petrus.d.r.soge@gmail.com>
2022-12-04 00:21:32 +0700

    2nd & 3rd solution

--- freecodecamp/Basic Algorithm Scripting/confirm-the-ending.js
+++ freecodecamp/Basic Algorithm Scripting/confirm-the-ending.js
@@ -31,4 +31,25 @@ function confirmEnding(str, target) {
 }
 
 console.log(confirmEnding("Bastian", "n"));
+
+Code Explanation:
+We need to make a pattern from the target variable that exists at the end of the string str.
+Since we will use a variable that will change the pattern each time the function is called, we will use the constructor of the regular expression object new RegExp(pattern[, flags]), so we start with: new RegExp(target).
+Then we have to check at the end of the string, so we concatenate to the target variable the $ character to match the end: new RegExp(target+'$').
+We use the flag i to ignore the case of the pattern and we have our completed RegExp: new RegExp(target+'$','i'), or we can ommit the flag entirely.
+Finally, we are using our regular expression with the test method to the given string, to check if the string ends with the pattern and return true or false accordingly.
+*/
+
+//OR
+
+/*
+function confirmEnding(str, target) {
+  return str.slice(-target.length) === target
+}
+
+confirmEnding("Bastian", "n");
+
+Code Explanation
+If a negative number is provided as the first parameter to slice() , the offset is taken backwards from the end of the string.
+
 */
