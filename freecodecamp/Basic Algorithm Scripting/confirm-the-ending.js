9595826682cb8c03e3328176b4b350cb247f9e91
dhekoz <petrus.d.r.soge@gmail.com>
2022-12-04 00:20:14 +0700

    2nd solution

--- freecodecamp/Basic Algorithm Scripting/confirm-the-ending.js
+++ freecodecamp/Basic Algorithm Scripting/confirm-the-ending.js
@@ -6,3 +6,29 @@ function confirmEnding(str, target) {
 }
 
 confirmEnding("He has to give me a new name", "name");
+
+/*
+Code Explanation
+First we use the slice method copy the string.
+In order to get the last characters in str equivalent to the target's length we use the slice method.
+The first parameter inside the slice method is the starting index and the second parameter would be the ending index.
+For example str.slice(10, 17) would return give me.
+In this case we only include one parameter which it will copy everything from the starting index.
+We substract the length of str and the length of target, that way, we shall get the last remaining characters equivalent to the target's length.
+Finally we compare the return result of slice to target and check if they have the same characters.
+*/
+
+//OR
+
+/*
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+
+  let re = new RegExp(target + "$", "i");
+
+  return re.test(str);
+}
+
+console.log(confirmEnding("Bastian", "n"));
+*/
