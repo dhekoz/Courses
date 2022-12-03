fbe397aee2313957547999a78aacd22e79366b95
dhekoz <petrus.d.r.soge@gmail.com>
2022-12-03 23:05:51 +0700

    return largest numbers in arrays 2 solution

--- freecodecamp/Basic Algorithm Scripting/return-largest-numbers-in-arrays.js
+++ freecodecamp/Basic Algorithm Scripting/return-largest-numbers-in-arrays.js
@@ -19,6 +19,30 @@ function largestOfFour(arr) {
 
   return results;
 }
+/*
+Create a variable to store the results as an array.
+Create an outer loop to iterate through the outer array.
+Create a second variable to hold the largest number and initialise it with the first number. This must be outside an inner loop so it won’t be reassigned until we find a larger number.
+Create said inner loop to work with the sub-arrays.
+Check if the element of the sub array is larger than the currently stored largest number. If so, then update the number in the variable.
+After the inner loop, save the largest number in the corresponding position inside of the results array.
+And finally return said array.
+*/
 
 //OR
+/*
+function largestOfFour(arr) {
+  return arr.map(function(group) {
+    return group.reduce(function(prev, current) {
+      return current > prev ? current : prev;
+    });
+  });
+}
 
+Code explanation
+we map all items within the main array to a new array using Array.prototype.map() and return this array as the final result
+within each inner array, we reduce its contents down to a single value using Array.prototype.reduce()
+the callback function passed to the reduce method takes the previous value and the current value and compares the two values
+if the current value is higher than the previous value we set it as the new previous value for comparison with the next item within the array or returns it to the map method callback if it’s the last item
+
+*/
