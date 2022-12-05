18c3ff060d6d42175b21e05689fc1a2d6e6fa71f
dhekoz <petrus.d.r.soge@gmail.com>
2022-12-05 08:49:16 +0700

    repeat-a-string-repeat-a-string 2nd solution

--- freecodecamp/Basic Algorithm Scripting/repeat-a-string-repeat-a-string.js
+++ freecodecamp/Basic Algorithm Scripting/repeat-a-string-repeat-a-string.js
@@ -11,3 +11,16 @@ function repeatStringNumTimes(str, num) {
 }
 
 repeatStringNumTimes("abc", 3);
+
+//OR
+/*
+function repeatStringNumTimes(string, times) {
+  if(times <= 0) 
+    return "";
+  if(times === 1) 
+    return string;
+  else 
+    return string + repeatStringNumTimes(string, times - 1);
+}
+repeatStringNumTimes("abc", 3);
+*/
