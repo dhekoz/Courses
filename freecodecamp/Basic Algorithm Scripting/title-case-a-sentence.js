8421110dc3d2da314ef490a98adc927a519c1268
dhekoz <petrus.d.r.soge@gmail.com>
2022-12-07 23:49:30 +0700

    title-case-a-sentences

--- freecodecamp/Basic Algorithm Scripting/title-case-a-sentence.js
+++ freecodecamp/Basic Algorithm Scripting/title-case-a-sentence.js
@@ -6,3 +6,9 @@ function titleCase(str) {
   }
   return updatedTitle.join(" ");
 }
+
+/*
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting words like the and of.
+*/
