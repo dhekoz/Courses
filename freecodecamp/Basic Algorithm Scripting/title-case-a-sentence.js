45987e76353e2d30541a92e78057fb126cb1cfea
dhekoz <petrus.d.r.soge@gmail.com>
2022-12-08 09:00:20 +0700

    test

--- freecodecamp/Basic Algorithm Scripting/title-case-a-sentence.js
+++ freecodecamp/Basic Algorithm Scripting/title-case-a-sentence.js
@@ -12,3 +12,15 @@ Return the provided string with the first letter of each word capitalized. Make 
 
 For the purpose of this exercise, you should also capitalize connecting words like the and of.
 */
+
+//OR
+/*
+function titleCase(str) {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map(val => val.replace(val.charAt(0), val.charAt(0).toUpperCase()))
+    .join(" ");
+}
+titleCase("I'm a little tea pot");
+*/
