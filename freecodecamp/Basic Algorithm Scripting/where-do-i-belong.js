6239a958b3e985545232d21a1b7a1f2a0975417c
dhekoz <petrus.d.r.soge@gmail.com>
2022-12-24 08:51:27 +0700

    solutions

--- freecodecamp/Basic Algorithm Scripting/where-do-i-belong.js
+++ freecodecamp/Basic Algorithm Scripting/where-do-i-belong.js
@@ -13,4 +13,25 @@ getIndexToIns([40, 60], 50);
   return arr.filter(val => num > val).length;
 }
 getIndexToIns([40, 60], 50);
+
+OR
+function getIndexToIns(arr, num) {
+  // sort and find right index
+  let index = arr
+    .sort((curr, next) => curr - next)
+    .findIndex(currNum => num <= currNum);
+  // Returns index or total length of arr
+  return index === -1 ? arr.length : index;
+}
+getIndexToIns([40, 60], 500);
+
+OR
+function getIndexToIns(arr, num) {
+  return arr
+    .concat(num)
+    .sort((a, b) => a - b)
+    .indexOf(num);
+}
+
+getIndexToIns([1, 3, 4], 2);
 */
