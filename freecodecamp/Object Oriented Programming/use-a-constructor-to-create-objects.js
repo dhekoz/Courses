c5a63d46ba02154788cc6e789679ea161daba52a
dhekoz <petrus.d.r.soge@gmail.com>
2022-12-25 22:49:17 +0700

    use-a-constructor-to-create-objects

---
+++ freecodecamp/Object Oriented Programming/use-a-constructor-to-create-objects.js
@@ -0,0 +1,15 @@
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+// Only change code below this line
+
+let hound = new Dog();
+hound.name = "Bibi";
+hound.color = "Yelowish";
+hound.numLegs = 4;
+
+/*
+Use the Dog constructor from the last lesson to create a new instance of Dog, assigning it to a variable hound.
+*/
