bbbac4b8147aa6f6188f9d86c790bdb47a698027
dhekoz <petrus.d.r.soge@gmail.com>
2022-12-26 07:05:26 +0700

    understand-where-an-objects-prototype-comes-from

--- freecodecamp/Object Oriented Programming/remember-to-set-the-constructor-property-when-changing-the-prototype.js
+++ freecodecamp/Object Oriented Programming/remember-to-set-the-constructor-property-when-changing-the-prototype.js
@@ -6,10 +6,10 @@ function Dog(name) {
 Dog.prototype = {
   constructor: Dog,
   numLegs: 4,
-  eat: function() {
+  eat: function () {
     console.log("nom nom nom");
   },
-  describe: function() {
+  describe: function () {
     console.log("My name is " + this.name);
-  }
+  },
 };
