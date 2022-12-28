b17f1ca5e90a472fe3b8dbdf48280dd87542b474
dhekoz <petrus.d.r.soge@gmail.com>
2022-12-28 14:48:37 +0700

    add

--- freecodecamp/Object Oriented Programming/change-the-prototype-to-a-new-object.js
+++ freecodecamp/Object Oriented Programming/change-the-prototype-to-a-new-object.js
@@ -4,11 +4,14 @@ function Dog(name) {
 
 Dog.prototype = {
   // Only change code below this line
-numLegs: 4,
-eat: function(){
-  console.log("nyam nyam nyam")
-},
-describe: function(){
-  console.log("My name is " + this.name);
-}
+  numLegs: 4,
+  eat: function () {
+    console.log("nyam nyam nyam");
+  },
+  describe: function () {
+    console.log("My name is " + this.name);
+  },
 };
+/*
+Add the property numLegs and the two methods eat() and describe() to the prototype of Dog by setting the prototype to a new object.
+*/
